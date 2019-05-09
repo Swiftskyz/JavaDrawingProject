30689f60f09c36635ed823ec3ef44e981771358b
Skylar <skylarhimeno@gmail.com>
2019-05-09 08:06:47 -0600

    added the imports in ArtPanel

--- src/drawing/view/ArtPanel.java
+++ src/drawing/view/ArtPanel.java
@@ -1,11 +1,29 @@
 package drawing.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import drawing.controller.DrawingController;
 
 public class ArtPanel extends JPanel
 {
 	private DrawingController app;
+	private Color currentColor;
+	private BufferedImage currentCanvas;
+	private int previousX;
+	private int previousY;
 	
 	public ArtPanel(DrawingController app)
 	{
